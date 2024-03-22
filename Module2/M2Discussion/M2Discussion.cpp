4a1a17ebc27d881866f03ad9b37ee63075e514c2
Ngi Bujri <nbujri7@gmail.com>
2024-03-21 22:01:54 -0400

    finish module 2 discussion program

---
+++ Module2/M2Discussion/M2Discussion.cpp
@@ -0,0 +1,33 @@
+/*
+    Student Name: Ngi Bujri
+    File Name: M2Discussion
+    Date: 03/21/2024
+    Description: simple input/output program with errors purposely included.
+*/
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    double 1stNumber;
+    double secondNumber;
+
+    cout << "\n\n  Enter a number: ";
+    cin >> 1stNumber;
+
+    cout << "\n  Enter a second number: ";
+    cin >> secondNumber;
+
+    sum = 1stNumber + secondNumber;
+
+    cout << "\n  The sum of " << 1stNumber << " and " << secondNumber << " is " << sum << endl;
+    cout << "\n  Press any key to continue..." << endl;
+
+    _getch();
+
+    return 0;
+}
+
